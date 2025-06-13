fcf332709a62a50a674de901b36a42e59079742b
Bilal Akhtar <nobodyneo666@gmail.com>
2025-06-13 13:35:08 +0500

    c

---
+++ htmx/realtime_poll/go.mod
@@ -0,0 +1,3 @@
+module github.com/belalakhter/cool_projects/htmx_poll
+
+go 1.23.3
---
+++ htmx/realtime_poll/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+type Candidate struct {
+	ID       int
+	Name     string
+	Position string
+	Count    int
+}
+
+var (
+	candidates []Candidate
+	nextID     = 1
+)
+
+func main() {
+	tmpl := template.Must(template.ParseGlob("views/*.html"))
+	t := &Templates{templates: tmpl}
+	http.HandleFunc("/", t.handleIndex)
+	fs := http.FileServer(http.Dir("views"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/modal", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("views/form.html"))
+		tmpl.Execute(w, nil)
+	})
+	http.HandleFunc("/add-candidate", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		name := r.FormValue("name")
+		position := r.FormValue("position")
+
+		password := r.FormValue("password")
+		if password != "admin" {
+			http.Error(w, "Wrong Password", http.StatusBadRequest)
+			return
+		}
+
+		if name == "" || position == "" {
+			http.Error(w, "Missing name or position", http.StatusBadRequest)
+			return
+		}
+
+		candidate := Candidate{
+			ID:       nextID,
+			Name:     name,
+			Position: position,
+			Count:    0,
+		}
+		nextID++
+		candidates = append(candidates, candidate)
+
+		tmpl := template.Must(template.ParseFiles("views/card.html"))
+		tmpl.Execute(w, candidate)
+	})
+	http.HandleFunc("/vote/", func(w http.ResponseWriter, r *http.Request) {
+		idStr := strings.TrimPrefix(r.URL.Path, "/vote/")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+
+		for i := range candidates {
+			if candidates[i].ID == id {
+				candidates[i].Count++
+				fmt.Fprintf(w, `<p id="count-%d" class="card-count">Count: %d</p>`, id, candidates[i].Count)
+				return
+			}
+		}
+
+		http.Error(w, "Candidate not found", http.StatusNotFound)
+	})
+
+	fmt.Printf("Server started on http://localhost:8080\n")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (t *Templates) handleIndex(w http.ResponseWriter, r *http.Request) {
+	err := t.templates.ExecuteTemplate(w, "index.html", candidates)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
